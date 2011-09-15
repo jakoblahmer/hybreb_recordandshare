828c26814529cf44567c0a271ecac3552d9d3a9d
Matthias Steinböck <grillen@abendstille.at>
2011-09-15 11:30:21 +0200

    plugin changes

--- hybreb_wp/wp-content/plugins/hybreb_hartenacht/fetchVideos.php
+++ hybreb_wp/wp-content/plugins/hybreb_hartenacht/fetchVideos.php
@@ -1,5 +1,84 @@
 <?php
 
-	echo 'i am an example';
+/** Loads the WordPress Environment and Template */
+ob_start();
 
-?>
+define('WP_USE_THEMES', false);
+require_once('../../../wp-load.php');
+wp();
+get_header();
+
+ob_end_clean();
+
+error_reporting(E_ALL);
+ini_set('display_errors', true);
+
+/** video config **/
+if(!defined('DS')) {
+	define('DS', DIRECTORY_SEPARATOR);
+}
+define('PHPVIDEOTOOLKIT_FFMPEG_BINARY', '/usr/bin/ffmpeg');
+require_once 'phpvideotoolkit/phpvideotoolkit.php5.php';
+$toolkit = new PHPVideoToolkit('/tmp');
+/** END video config **/
+
+
+
+$number = (int)$_GET['videos'];
+if ($number <= 0) $number = 10;
+
+
+$args = array(
+	'post_type' => 'post'
+,	'post_status' => null
+,	'numberposts' => $number
+);
+$attachArgs = array(
+	'post_type' => 'attachment'
+,	'numberposts' => 1
+,	'post_status' => null
+,	'post_parent' => null
+);
+
+$json = array();
+$uploads = wp_upload_dir();
+
+$posts = get_posts($args);
+if ($posts) {
+	//echo 'we got '.count($posts).' posts<br />';
+	foreach ($posts as $post) {
+		$oPost = $post;
+		$attachArgs['post_parent'] = $post->ID;
+		
+		$attachments = get_posts($attachArgs);
+		
+		if (!$attachments || count($attachments) <= 0) continue;
+		
+		setup_postdata($attachments[0]);
+		
+		$url = wp_get_attachment_url($attachments[0]->ID, false);
+		
+		$filePath = $uploads['basedir'].substr($url, strlen($uploads['baseurl']));
+		
+		$ok = $toolkit->setInputFile($filePath);
+		if(!$ok)
+		{
+// 			if there was an error then get it 
+			echo $toolkit->getLastError()."<br /><br />\r\n";
+			$toolkit->reset();
+			continue;
+		}
+		
+		$info = $toolkit->getFileInfo();
+		
+		$json[] = array(
+			'name' => apply_filters( 'the_title', $attachments[0]->post_title )
+		,	'author' => get_the_author()
+		,	'length' => $info['duration']['timecode']['rounded']
+		,	'url' => $url
+		);
+	}
+}
+
+header('Content-Type: application/json');
+echo json_encode($json);
--- hybreb_wp/wp-content/plugins/hybreb_hartenacht/hybreb_hartenacht.php
+++ hybreb_wp/wp-content/plugins/hybreb_hartenacht/hybreb_hartenacht.php
@@ -13,19 +13,8 @@ License:
 class Hybreb_HarteNacht	{
 	
 	public static function init() {
-		
-		
-		// embed the javascript file that makes the AJAX request
-		//wp_enqueue_script( 'a1social', plugin_dir_url( __FILE__ ) . 'js/a1socialmedia.js', array( 'jquery' ) );
- 
-		// declare the URL to the file that handles the AJAX request (wp-admin/admin-ajax.php)
-		//wp_localize_script( 'a1social', 'A1SocialMedia', array( 'ajaxurl' => admin_url( 'admin-ajax.php' ) ) );
-		
-		
 		self::addFilters();
 		self::addActions();
-		// multilang
-//		load_plugin_textdomain('A1_Author_SocialMedia', false, dirname(plugin_basename(__FILE__ )));
 		return true;
 	}
 	
@@ -36,6 +25,7 @@ class Hybreb_HarteNacht	{
 	 */
 	public static function doRewriteRules() {
 		add_rewrite_rule('list-videos/(.*)/?$', 'wp-content/plugins/hybreb_hartenacht/fetchVideos.php?videos=$1', 'top');
+		add_rewrite_rule('add-video/(.*)/?$', 'wp-content/plugins/hybreb_hartenacht/addVideo.php?userId=$1', 'top');
 		flush_rewrite_rules(true);
 	}
 
@@ -47,18 +37,10 @@ class Hybreb_HarteNacht	{
 	 * adds the filters
 	 */
 	private static function addFilters() {
-		// add input fields
-		// add_filter('query_vars', array('videos'));
+		// no filters for this plugin
 	}
 
 	private static function addActions() {
-		//add_action('edit_user_profile', array('A1_Author_SocialMedia', 'addSpecialSocialMedia'));
-		//add_action('show_user_profile', array('A1_Author_SocialMedia', 'addSpecialSocialMedia'));
-		//add_action('profile_update', array('A1_Author_SocialMedia', 'saveSpecialSocialMedia'), 10, 1);
-		
-		//add_action('admin_print_styles', array('A1_Author_SocialMedia', 'my_admin_styles'));
-		//add_action('a1_socialmedia_print', array('A1_Author_SocialMedia', 'print_socialMedia'));
-		
 		register_activation_hook(__FILE__, array('Hybreb_HarteNacht', 'doRewriteRules'));
 		register_deactivation_hook(__FILE__, array('Hybreb_HarteNacht', 'removeRewriteRules'));
 	}
